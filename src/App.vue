4a75df7d154315a9af3e64afaf84ab3691b43e2c
Abdallah Hegab <abdallahhegab192@gmail.com>
2022-06-21 13:39:35 +0200

    lab2 vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <NavComponent/>
+  <router-view/>
+</template>
+
+<script>
+
+import NavComponent from "@/components/NavComponent";
+export default {
+  name: 'App',
+  components: {
+    NavComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
